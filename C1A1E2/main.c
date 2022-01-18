f1824eb13c18d39f2157667b17a132f0e29a24fc
pauldefusco <pauldefusco@cloudera.com>
2022-01-17 20:21:35 -0800

    Cleaned up formatting to avoid warnings

--- C1A1E2/main.c
+++ C1A1E2/main.c
@@ -7,16 +7,15 @@
 
 #include <stdio.h>
 
-int main() {
-    // insert code here...
-
-        printf("Poorly formatted code is a red flag to employers.\n"
-        "\"Good\" programmers format code 100%% correctly.\n"
-        "The \"newline\" character is represented by \\n.\n"
-        "Five backslashes: \\\\\\\\\\\n"
-        "Six double quotes: \"\"\"\"\"\"\n"
-        "Seven percent signs: %%%%%%%%%%%%%%\n"
-        "No partridge and no pear tree!\n");
-        
-        return 0;
+int main(void) {
+    
+    printf("Poorly formatted code is a red flag to employers.\n"
+    "\"Good\" programmers format code 100%% correctly.\n"
+    "The \"newline\" character is represented by \\n.\n"
+    "Five backslashes: \\\\\\\\\\\n"
+    "Six double quotes: \"\"\"\"\"\"\n"
+    "Seven percent signs: %%%%%%%%%%%%%%\n"
+    "No partridge and no pear tree!\n");
+    
+    return 0;
 }
