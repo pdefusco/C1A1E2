36b9cdbd41016d74a926f4a7b1f945b558c44cf3
pauldefusco <pauldefusco@cloudera.com>
2022-01-17 19:31:25 -0800

    C1A1E2

--- C1A1E2/main.c
+++ C1A1E2/main.c
@@ -9,6 +9,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+        printf("Poorly formatted code is a red flag to employers.\n"
+        "\"Good\" programmers format code 100%% correctly.\n"
+        "The \"newline\" character is represented by \\n.\n"
+        "Five backslashes: \\\\\\\\\\\n"
+        "Six double quotes: \"\"\"\"\"\"\n"
+        "Seven percent signs: %%%%%%%%%%%%%%\n"
+        "No partridge and no pear tree!");
+        
+        return 0;
 }
