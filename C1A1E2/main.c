70d0e99048c29f62d53de66f9b969ed8103fca87
pauldefusco <pauldefusco@cloudera.com>
2022-01-17 19:18:53 -0800

    Initial Commit

---
+++ C1A1E2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C1A1E2
+//
+//  Created by Paul de Fusco on 1/17/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
